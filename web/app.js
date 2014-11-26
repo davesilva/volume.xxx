e9f46fe81259856e5e8bd21a94f6847d67ed0edf
David Silva <dsilva917@gmail.com>
2014-11-26 00:53:51 -0500

    Change the volume field to a spinner

---
+++ web/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    var $power = $("input[name=power]");
+    var $volume = $("input[name=volume]");
+    var $form = $('form');
+
+    var getSpeakerState = function() {
+        $.ajax("/speakers").done(function(response) {
+            $power.val([response['power']]);
+            $volume.val(response['volume']);
+        });
+    };
+
+    $volume.TouchSpin({
+        min: 0,
+        max: 50,
+        step: 1,
+        booster: false,
+        mousewheel: false
+    });
+
+    $volume.change(function(event) {
+        $power.val([true]);
+    });
+
+    $volume.on("mouseup", function(event) {
+        $volume.select();
+    });
+
+    $('input').change(function(event) {
+        $form.submit();
+    });
+
+    getSpeakerState();
+
+    $volume.focus();
+    $('input[type=submit]').hide();
+});
